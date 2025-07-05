7dd64562b4fa066a25cbbc76965172359babcdda
Dimas <dimasriat@gmail.com>
2025-07-05 13:34:30 +0700

    feat: add new address

--- indexer/ponder.config.ts
+++ indexer/ponder.config.ts
@@ -13,20 +13,20 @@ export default createConfig({
     ProjectFactory: {
       chain: "arbitrumSepolia",
       abi: ProjectFactoryABI,
-      address: "0xD9948702FF4a56EfD553Dab9b266225Dc5FBc9C2",
-      startBlock: 170171022, 
+      address: "0x01ebD170A7D3eCb6d105Cea1f37f34F89BE4dc97",
+      startBlock: 170213392, 
     },
     DirectPoolTemplate: {
       chain: "arbitrumSepolia", 
       abi: DirectPoolABI,
-      address: "0x782b3ad3bAE6E12389cDa5C004C05967f2DEC55b",
-      startBlock: 170170991,
+      address: "0x41DBC5C299A9C31F0A0A32aaE8B7c4dd76bB5014",
+      startBlock: 170213364,
     },
     BondingCurveTemplate: {
       chain: "arbitrumSepolia",
       abi: BondingCurveABI, 
-      address: "0xA3976D0bDd4714f91027c2d589B7F9419373Be60",
-      startBlock: 170171007,
+      address: "0x4FcFc18A2215cEBDfBd1AC30A1576E267237983B",
+      startBlock: 170213378,
     },
   },
 });
